a09a266fdd65fce829556c3b62b935ffda5b1696
ombasaabuya <ombasabuya@gmail.com>
2023-03-17 08:20:04 +0300

    completed

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,9 +1,19 @@
 class CheesesController < ApplicationController
+
+
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
 
   def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
   end
 end
+
+
+
+
+
+
